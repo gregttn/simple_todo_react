fb0445f582119eb1ed3d70ff9e182f6e512b16c7
greg <gregttn@gmail.com>
2018-04-17 09:15:18 +0100

    Wired redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './style/index.css';
 import Todos from './components/todos';
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './reducers/index';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <Todos/>
+  <Provider store={store}>
+    <Todos/>
+  </Provider>
   ,document.querySelector('.container'));
 registerServiceWorker();
